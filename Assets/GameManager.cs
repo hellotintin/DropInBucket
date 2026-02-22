2b8e68a515b89480e4985b2ed6f72be90826f81a
Tintin <113481508+hellotintin@users.noreply.github.com>
2026-02-22 16:36:43 +0800

    Initial Assets
    Initial Assets

---
+++ Assets/GameManager.cs
@@ -0,0 +1,63 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+    public static GameManager instance;
+    Camera cam;
+    DraggableObj draggingObj;
+    Vector2 dragOffset;
+
+    public Vector2 MousePos
+    {
+        get { return cam.ScreenToWorldPoint(Input.mousePosition); }
+    }
+
+    void Awake()
+    {
+        if (instance == null)
+            instance = this;
+        else
+            Destroy(gameObject);
+    }
+
+    void Start()
+    {
+        cam = Camera.main;
+    }
+
+    void Update()
+    {
+        if (Input.GetMouseButtonDown(0) && draggingObj == null)
+        {
+            RaycastHit2D _hit = Physics2D.Raycast(MousePos, Vector2.zero);
+            if (_hit.collider != null)
+            {
+                if (_hit.collider.GetComponent<DraggableObj>() != null)
+                {
+                    draggingObj = _hit.collider.GetComponent<DraggableObj>();
+                    dragOffset = MousePos - (Vector2)draggingObj.transform.position;
+                }
+            }
+        }
+
+        if (draggingObj != null)
+        {
+            draggingObj.transform.position = MousePos - dragOffset;
+
+            if (draggingObj.canRotate)
+            {
+                const float rotSpeed = 180;
+                if (Input.GetKey(KeyCode.LeftArrow))
+                    draggingObj.transform.Rotate(Vector3.forward * rotSpeed * Time.deltaTime);
+                else if (Input.GetKey(KeyCode.RightArrow))
+                    draggingObj.transform.Rotate(Vector3.forward * -rotSpeed * Time.deltaTime);
+            }
+
+
+            if (!Input.GetMouseButton(0))
+                draggingObj = null;
+        }
+    }
+}
---
+++ Assets/WaterObj.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WaterObj : MonoBehaviour
+{
+    ParticleSystem ps;
+    [SerializeField] List<ParticleSystem.Particle> enterList = new List<ParticleSystem.Particle>();
+
+    void Start()
+    {
+        ps = GetComponent<ParticleSystem>();
+    }
+
+    void OnParticleTrigger() 
+    {
+        int numEnter = ps.GetTriggerParticles(ParticleSystemTriggerEventType.Enter, enterList);
+
+        // iterate
+        for (int i = 0; i < numEnter; i++)
+        {
+            ParticleSystem.Particle p = enterList[i];
+            p.remainingLifetime = 0f;
+            enterList[i] = p;
+
+            Debug.Log("Fill!");
+        }
+
+        // set
+        ps.SetTriggerParticles(ParticleSystemTriggerEventType.Enter, enterList);
+	}
+}
